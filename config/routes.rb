09e062f8f45ae301676fb6359496407737bac4f7
saenokami <165577235+saenokami@users.noreply.github.com>
2024-05-06 01:59:30 +0000

    [Add]いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users, only: [:index,:show,:edit,:update]
-  resources :books
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+   resource :favorites, only: [:create, :destroy]
+ end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end 
